a2e18e0f9285adf40d191ed80bc24e9683e42836
maulan909 <maulanahilmi909@gmail.com>
2022-06-24 19:09:25 +0700

    hotfix: fitur delete

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './List.css'
 
 const List = (props) => {
+    const deleteItem = (index) => {
+        props.onDeleteList(index)
+    }
     return (
         <li className='item'>
             <div className='checkbox'>
@@ -10,7 +13,7 @@ const List = (props) => {
             <div className='value-item'>
                 {props.item}
             </div>
-            <a href='#' className='close-button'>
+            <a href='#' className='close-button' onClick={() => deleteItem(props.index)}>
                 
             </a>
         </li>
--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -7,16 +7,21 @@ import {useState} from 'react';
 const Todo = () => {
     const [input, setInput] = useState('');
     const [list, setList] = useState([]);
+    const deleteList = (index) => {
+        const current = list.slice()
+        current.splice(index, 1)
+        setList([...current]);
+    }
     return (
         <div>
             <h1>Simple To do list app</h1>
             <div className='input-field'>
                 <input type="text" className='input' onChange={(event) =>  setInput(event.target.value)} />
-                <input type="submit" className='button-add' value='Tambah' onClick={() => setList([...list, input])} />
+                <input type="submit" className='button-add' value='Tambah' onClick={() => input.length > 0 ? setList([...list, {status:false, value:input}]) : undefined} />
             </div>
             <div>
                 <ul className='list'>
-                    {list.map((item) => (<List item={item} />))}
+                    {list.map((item, index) => (<List item={item.value} index={index} onDeleteList={deleteList} />))}
                 </ul>
             </div>
         </div>
